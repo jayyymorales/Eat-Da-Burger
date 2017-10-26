ac0214969939d6b55b99a61788ba793d01264d6b
jayyymorales <jrmorales@ymail.com>
2017-10-26 00:39:15 -0700

    heroku update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = process.env.PORT || 8020;
+var port = process.env.PORT || 3000;
 
 var app = express();
 app.use(express.static("public"));
